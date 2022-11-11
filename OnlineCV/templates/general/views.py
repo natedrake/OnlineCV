520811f3365c78e90ea4c0118440a15052b972f9
John O'Grady <johograd@amazon.com>
2022-11-11 00:10:39 +0000

    Add health check URL

--- OnlineCV/templates/general/views.py
+++ OnlineCV/templates/general/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """General views."""
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 
 general_blueprint = Blueprint('general', __name__, url_prefix='/',
     template_folder='../general/')
@@ -8,3 +8,11 @@ general_blueprint = Blueprint('general', __name__, url_prefix='/',
 @general_blueprint.route('/')
 def index():
     return render_template('index.html', title="Resume")
+
+@general_blueprint.route('/health', methods=['GET', 'POST'])
+@general_blueprint.route('/health/<string:type>', methods=['POST'])
+def health(type=None):
+    if type == 'json':
+        return jsonify(message='OK')
+    else:
+        return render_template('health.html', title='Health Check')
