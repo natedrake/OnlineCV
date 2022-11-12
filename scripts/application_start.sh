697bef0be9bcdff2a53050de2667124097955001
John O'Grady <johograd@amazon.com>
2022-11-12 13:06:44 +0000

    Start waitress-serve as background process

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd /home/ubuntu/OnlineCV
-waitress-serve --listen=0.0.0.0:80 --threads=8 --call 'autoapp:create_app'
+nohup waitress-serve --listen=0.0.0.0:80 --threads=8 --call 'autoapp:create_app' 1>/dev/null &
