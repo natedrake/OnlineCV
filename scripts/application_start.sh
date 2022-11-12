4d8b0259cf65f57bd7edc16c8f91423a0673de5d
John O'Grady <johograd@amazon.com>
2022-11-12 11:50:16 +0000

    Removed dot from foldername

---
+++ scripts/application_start.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd /home/ubuntu/OnlineCV
+/home/ubuntu/.local/bin/flask run --host=0.0.0.0 --port=80 --with-threads
---
+++ scripts/application_stop.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+kill $(pgrep -f flask)
